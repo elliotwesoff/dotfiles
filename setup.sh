f2a816ca3340be28b561c23fe61e8fd4a0898506
Elliot Wesoff <elliot.wesoff@gmail.com>
2022-05-11 20:22:08 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-
-# TODO: symlinking the entire .config folder may be bad.
-# maybe we should symlink only the subdirectories we care about?
 
 mv ~/.config ~/.config.old 2> /dev/null
 mv ~/.fonts ~/.fonts.old 2> /dev/null
 mv ~/.gitconfig ~/.gitconfig.old 2> /dev/null
+mv ~/.agignore ~/.agignore.old 2> /dev/null
 mv ~/.tmux.conf ~/.tmux.conf.old 2> /dev/null
 mv ~/.zshrc ~/.zshrc.old 2> /dev/null
 
 ln -s $SCRIPT_DIR/.config ~/.config
 ln -s $SCRIPT_DIR/.fonts ~/.fonts
-ln $SCRIPT_DIR/.gitconfig ~/.gitconfig
-ln $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
-ln $SCRIPT_DIR/.zshrc ~/.zshrc
-ln -s $SCRIPT_DIR/.config/nvim/init.vim ~/.nvimrc 
+ln -s $SCRIPT_DIR/.agignore ~/.agignore
+ln -s $SCRIPT_DIR/.gitconfig ~/.gitconfig
+ln -s $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
+ln -s $SCRIPT_DIR/.zshrc ~/.zshrc
 
 touch ~/.env-vars
 chmod 600 ~/.env-vars
