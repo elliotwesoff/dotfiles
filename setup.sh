f02589bd2a1d8a8e6cd30844b16b892824163ebe
Elliot Wesoff <ewesoff@gmail.com>
2022-07-20 06:15:26 +0200

    add .xinitrc, few minor updates.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-for item in .config .local .fonts .gitconfig .agignore .tmux.conf .zshrc
+for item in .config .local .fonts .gitconfig .agignore .tmux.conf .zshrc .xinitrc
 do
-  echo "moving\t\t~/$item\t=> ~/$item.old"
-  mv ~/$item ~/$item.old 2> /dev/null
+  # echo "moving\t\t~/$item\t=> ~/$item.old"
+  mv -v ~/$item ~/$item.old 2> /dev/null
   echo "symlinking\t~/$item\t=> ~/dotfiles/$item\n"
   ln -s ~/dotfiles/$item ~/$item
 done
