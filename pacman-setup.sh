d8daa11ac481219ffe692a184ff473d145fa9025
Elliot Wesoff <ewesoff@gmail.com>
2022-09-14 21:42:35 -0700

    add pacman and yay setup scripts.

---
+++ pacman-setup.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+echo installing pacman packages...
+
+sudo pacman -S bspwm \
+  nvim \
+  git \
+  kitty \
+  sxhkd \
+  neovim \
+  picom \
+  vivaldi \
+  tmux \
+  rofi \
+  zsh \
+  feh \
+  arandr \
+  wget \
+  deno \
+  nautilus \
+  polybar \
+  libnotify \
+  xorg-server \
+  xorg-xinit \
+  mesa \
+  xorg-xev \
+  vulkan-intel \
+  pipewire \
+  pipewire-docs \
+  wireplumber \
+  wireplumber-docs \
+  alsa-utils \
+  alsa-firmware \
+  vivaldi-ffmpeg-codecs \
+  ffmpeg \
+  pavucontrol \
+  pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack \
+  alsa-utils \
+  mesa-utils \
+  man-db \
+  sway \
+  gdb \
+  ccls \
+  libqalculate qalculate-qt \
+  vscode \
+  libreoffice-fresh \
+  gnome-control-center \
+  plasma-meta kde-applications-meta kwin plasma-wayland-session
+
+
---
+++ yay-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo installing yay packages...
+
+yay -Sy nvim-packer-git \
+  noto-fonts-emoji \
+  pulseaudio-control \
+  river \
+  nvm \
+  1password \
+  discord
+
