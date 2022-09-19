1f4ae5948bcc8d43ff3d9ab79daab14871c30713
Elliot Wesoff <ewesoff@gmail.com>
2022-09-19 01:12:34 -0700

    trying to figure out dpi and such.

--- .screenlayout/x1-ext.sh
+++ .screenlayout/x1-ext.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-if [[ $(xrandr -q | grep -w "DP-3 connected") ]]
-then
-  xrandr \
-    --output eDP-1 --off \
-    --output DP-3 --mode 2560x1440 --rate 100
-fi
+# --output eDP-1 --off \
+xrandr \
+  --output eDP-1 --mode 2880x1800 --dpi 192 \
+  --output DP-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal --rate 100 --left-of eDP-1
 
--- .screenlayout/x1.sh
+++ .screenlayout/x1.sh
@@ -3,10 +3,12 @@
 # x1.sh
 
 # xrandr --output eDP-1 --mode 2880x1800 --pos 2560x327 --rotate normal --dpi 192 \
-xrandr --output eDP-1 --mode 1920x1200 --pos 2560x327 --rotate normal --dpi 192 \
+# --output DP-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal --rate 100 \
+xrandr \
+  --output eDP-1 --mode 1920x1200 --dpi 192 \
   --output HDMI-1 --off \
   --output DP-1 --off \
   --output DP-2 --off \
-  --output DP-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal --rate 100 \
+  --output DP-3 --off \
   --output DP-4 --off
 
