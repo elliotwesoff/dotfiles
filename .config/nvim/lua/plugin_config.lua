cb46d10553497314f2f938bb47f0badaa668a91e
Elliot Wesoff <ewesoff@gmail.com>
2022-10-21 10:26:13 -0700

    fixed plugin opts. whoops lol

--- .config/nvim/lua/plugin_config.lua
+++ .config/nvim/lua/plugin_config.lua
@@ -346,6 +346,23 @@ end
 local function apply_codewindow_config()
   local codewindow = require('codewindow')
   local opts = {
+    active_in_terminals = false, -- Should the minimap activate for terminal buffers
+    auto_enable = false, -- Automatically open the minimap when entering a (non-excluded) buffer (accepts a table of filetypes)
+    exclude_filetypes = {}, -- Choose certain filetypes to not show minimap on
+    max_minimap_height = nil, -- The maximum height the minimap can take (including borders)
+    max_lines = nil, -- If auto_enable is true, don't open the minimap for buffers which have more than this many lines.
+    minimap_width = 20, -- The width of the text part of the minimap
+    use_lsp = true, -- Use the builtin LSP to show errors and warnings
+    use_treesitter = true, -- Use nvim-treesitter to highlight the code
+    width_multiplier = 4, -- How many characters one dot represents
+    z_index = 1, -- The z-index the floating window will be on
+  }
+  codewindow.setup(opts)
+  codewindow.apply_default_keybinds()
+end
+
+local function apply_symbolsoutline_config()
+  local opts = {
     highlight_hovered_item = true,
     show_guides = true,
     auto_preview = false,
@@ -406,13 +423,7 @@ local function apply_codewindow_config()
       TypeParameter = {icon = "𝙏", hl = "TSParameter"}
     }
   }
-
-  codewindow.setup(opts)
-  codewindow.apply_default_keybinds()
-end
-
-local function apply_symbolsoutline_config()
-  require("symbols-outline").setup()
+  require("symbols-outline").setup(opts)
 end
 
 return {
