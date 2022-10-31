25b0d42aa4d6c56333de1d7fc7f500943503f0c5
Elliot Wesoff <ewesoff@gmail.com>
2022-10-31 01:57:05 -0700

    use nvim-cmp instead of ddc because ddc was not playing nicely with noice. noice is a bit noicer now.

--- .config/nvim/lua/dependencies.lua
+++ .config/nvim/lua/dependencies.lua
@@ -21,11 +21,11 @@ local function use_dependencies(use)
   use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
   use { 'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
   use { 'gorbit99/codewindow.nvim', config = plugins.apply_codewindow_config }
-  -- use({
-  --   'folke/noice.nvim',
-  --   config = plugins.apply_noice_config,
-  --   requires = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' }
-  -- })
+  use({
+    'folke/noice.nvim',
+    config = plugins.apply_noice_config,
+    requires = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' }
+  })
 
   -- ddc insanity
   use 'Shougo/ddc.vim'
@@ -37,6 +37,16 @@ local function use_dependencies(use)
   use 'Shougo/ddc-ui-native'
   use 'delphinus/ddc-treesitter'
   use 'matsui54/ddc-buffer'
+
+  -- nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+  use 'ray-x/cmp-treesitter'
 end
 
 return { use_dependencies = use_dependencies }
--- .config/nvim/lua/plugin_config.lua
+++ .config/nvim/lua/plugin_config.lua
@@ -51,6 +51,7 @@ end
 local function apply_lsp_config()
   local keymap_opts = { noremap = true, silent = true }
 
+
   -- See `:help vim.diagnostic.*` for documentation on any of the below functions
   vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, keymap_opts)
   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, keymap_opts)
@@ -86,15 +87,18 @@ local function apply_lsp_config()
 
   local lspconfig = require('lspconfig')
   local lsp_flags = { debounce_text_changes = 50 }
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
   lspconfig.pyright.setup {
-      on_attach = on_attach,
-      flags = lsp_flags,
+    on_attach = on_attach,
+    flags = lsp_flags,
+    capabilities = capabilities
   }
 
   lspconfig.tsserver.setup {
-      on_attach = on_attach,
-      flags = lsp_flags,
+    on_attach = on_attach,
+    flags = lsp_flags,
+    capabilities = capabilities
   }
 
   lspconfig.sumneko_lua.setup {
@@ -104,18 +108,25 @@ local function apply_lsp_config()
           globals = {"vim"},
         },
       },
-    }
+    },
+    capabilities = capabilities
   }
 
   lspconfig.ccls.setup {
     on_attach = on_attach,
-    flags = lsp_flags
+    flags = lsp_flags,
+    capabilities = capabilities
   }
 
   -- lspconfig.clangd.setup {
   --   on_attach = on_attach,
   --   flags = lsp_flags
   -- }
+
+  -- Set up lspconfig.
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  }
 end
 
 local function apply_lualine_config()
@@ -445,6 +456,103 @@ local function apply_notify_config()
   })
 end
 
+local function apply_nvim_cmp_config()
+  local cmp = require('cmp')
+  vim.cmd("set completeopt=menu,menuone,noselect")
+  local cmp_kinds = {
+    Text = '  ',
+    Method = '  ',
+    Function = '  ',
+    Constructor = '  ',
+    Field = '  ',
+    Variable = '  ',
+    Class = '  ',
+    Interface = '  ',
+    Module = '  ',
+    Property = '  ',
+    Unit = '  ',
+    Value = '  ',
+    Enum = '  ',
+    Keyword = '  ',
+    Snippet = '  ',
+    Color = '  ',
+    File = '  ',
+    Reference = '  ',
+    Folder = '  ',
+    EnumMember = '  ',
+    Constant = '  ',
+    Struct = '  ',
+    Event = '  ',
+    Operator = '  ',
+    TypeParameter = '  ',
+  }
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    }),
+    formatting = {
+      format = function(_, vim_item)
+        -- vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
+        vim_item.kind = (cmp_kinds[vim_item.kind] or '')
+        return vim_item
+      end,
+    }
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+end
+
 return {
   apply_ddc_config = apply_ddc_config,
   apply_lsp_config = apply_lsp_config,
@@ -459,5 +567,6 @@ return {
   apply_symbolsoutline_config = apply_symbolsoutline_config,
   apply_indent_blankline_config = apply_indent_blankline_config,
   apply_noice_config = apply_noice_config,
-  apply_notify_config = apply_notify_config
+  apply_notify_config = apply_notify_config,
+  apply_nvim_cmp_config = apply_nvim_cmp_config
 }
