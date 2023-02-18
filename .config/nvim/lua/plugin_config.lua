6f1a8c9047866c20c21b5e4a14601530bf63a04c
Elliot Wesoff <ewesoff@gmail.com>
2023-02-17 23:11:32 -0800

    update nvim glance config.

--- .config/nvim/lua/plugin_config.lua
+++ .config/nvim/lua/plugin_config.lua
@@ -477,7 +477,7 @@ function M.apply_glance_config()
     },
     list = {
       position = 'right', -- Position of the list window 'left'|'right'
-      width = 0.25, -- 33% width relative to the active window, min 0.1, max 0.5
+      width = 0.3, -- 33% width relative to the active window, min 0.1, max 0.5
     },
     theme = { -- This feature might not work properly in nvim-0.7.2
       enable = true, -- Will generate colors for the plugin based on your current colorscheme
