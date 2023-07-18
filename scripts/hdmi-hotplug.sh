a8c61bfed2a3350dcf2ba3031e814b1cd6c4d0bd
Elliot Wesoff <ewesoff@gmail.com>
2023-07-17 22:57:17 -0700

    add copy of udev rule so we can't lose it lol

--- scripts/hdmi-hotplug.sh
+++ scripts/hdmi-hotplug.sh
@@ -6,6 +6,9 @@
 # HDMI cable is plugged in or unplugged. (maybe 
 # also when a USB-C hub with HDMI is hotplugged?)
 # udev rule is in /etc/udev/rules.d/95-hdmi-plug.rules.
+#
+# in case the udev rule is ever lost, the rule is:
+# KERNEL=="card0", ACTION=="change", SUBSYSTEMS=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/elliot/.Xauthority", RUN+="/home/elliot/dotfiles/scripts/hdmi-hotplug.sh"
 
 echo HDMI hotplugged @ $(date) >> /home/elliot/tmp/hdmi-hotplug.log
 
