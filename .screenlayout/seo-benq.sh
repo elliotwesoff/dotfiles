ede7c72f104500ebca95a893fbfcb062cc3e65cb
Elliot Wesoff <ewesoff@gmail.com>
2023-07-25 16:31:39 +0200

    strikethrough volume when muted in eww main window.

--- .screenlayout/seo-benq.sh
+++ .screenlayout/seo-benq.sh
@@ -5,6 +5,6 @@ xrandr \
   --output eDP-1 --off \
   --output DP-1 --off \
   --output DP-2 --off \
-  --output DP-3 --primary --mode 1920x1080 --rotate normal \
+  --output DP-3 --primary --mode 1920x1080 --rate "74.97" --rotate normal \
   --output DP-4 --off
   # --output DP-4 --primary --mode 1920x1080 --rotate normal
