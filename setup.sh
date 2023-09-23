f997bdee894134165ed74c96955586a1fc69fd52
Elliot Wesoff <ewesoff@gmail.com>
2023-09-23 11:47:09 -0700

    migrate pacman-setup.sh and yay-setup.sh to setup.sh, remove some packages.

--- setup.sh
+++ setup.sh
@@ -11,11 +11,82 @@ done
 # symlink ~/tmp to the linux-provided in-memory filesystem.
 ln -sfv /dev/shm ~/tmp
 
-mkdir -p ~/.local/state
 mkdir -p ~/code
+mkdir -p ~/.local/state
 
 # install all the things!
-sh ./pacman-setup.sh
+sudo pacman -S \
+  base-devel \
+  linux-lts linux-lts-headers \
+  xorg-server xorg-xinit xorg-xinput xorg-xsetroot xorg-xev \
+  libnotify \
+  xdg-utils \
+  mesa mesa-utils \
+  pipewire pipewire-docs \
+  wireplumber wireplumber-docs \
+  alsa-utils alsa-firmware \
+  pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pavucontrol sof-firmware \
+  bluez bluez-utils \
+  cups cups-pdf \
+  noto-fonts noto-fonts-emoji \
+  bspwm \
+  vulkan-intel \
+  vim \
+  neovim \
+  git \
+  kitty \
+  zsh \
+  fish \
+  sxhkd \
+  pulsemixer \
+  zathura zathura-cb zathura-djvu zathura-pdf-mupdf \
+  pdftk \
+  tmux \
+  rofi \
+  feh \
+  arandr \
+  wget \
+  deno \
+  thunar \
+  ntfs-3g \
+  cmus \
+  vlc \
+  baobab \
+  ncdu \
+  ffmpeg \
+  man-db \
+  gdb \
+  ccls \
+  lua-language-server \
+  pyright \
+  libqalculate qalculate-qt \
+  font-manager \
+  texlive-core texlab \
+  pinta \
+  playerctl \
+  unrar \
+  qmk \
+  htop \
+  btop \
+  firefox \
+  rustup \
+  rclone \
+  xclip \
+  redshift \
+  dunst \
+  sxiv \
+  wmctrl \
+  dunst \
+  flameshot \
+  jq \
+  traceroute \
+  whois \
+  bind \
+  usbutils \
+  usbview \
+  xsecurelock \
+  xss-lock \
+  p7zip
 
 # install yay
 sudo pacman -S --needed git base-devel \
@@ -24,7 +95,22 @@ sudo pacman -S --needed git base-devel \
   && makepkg -si
 
 # install all the things not in the pacman repos!
-sh ./yay-setup.sh
+yay -S \
+  nvm \
+  nvim-packer-git \
+  1password \
+  discord \
+  cava \
+  xinit-xsession \
+  soulseekqt \
+  blight \
+  nordvpn-bin \
+  otf-intel-one-mono \
+  asdf-vm \
+  vscode-codicons-git \
+  spotify \
+  postman-bin \
+  intel-opencl-runtime
 
 # install eww
 cd ~/code \
