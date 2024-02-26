d105cb6378d0c10e86d319a7a90e9258c7b399c1
Elliot Wesoff <ewesoff@gmail.com>
2024-02-26 00:19:39 -0800

    add java vim ftplugin, adjust dashboard greeter and css a bit.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@ local lazy = require('lazy_config')
 local settings = require('settings')
 local keymaps = require('keymaps')
 
-settings.COLORSCHEME_LIGHT = 'catppuccin'
+settings.COLORSCHEME_LIGHT = 'dayfox'
 -- settings.COLORSCHEME_DARK = 'carbonfox'
 settings.COLORSCHEME_DARK = 'default'
 
@@ -11,3 +11,6 @@ lazy.init()
 
 settings.apply_settings()
 keymaps.apply_keymaps()
+
+-- vim.api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
