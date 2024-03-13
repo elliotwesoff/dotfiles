05abf4c844d3af7f02c2b0bd1f9d0a17169b02e1
Elliot Wesoff <ewesoff@gmail.com>
2024-03-12 23:55:21 -0700

    ts whitespace and add java jdtls to server list.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -32,6 +32,7 @@ return {
           capabilities = capabilities,
           on_attach = attach_fn
         }
+        lspconfig.jdtls.setup(lsp_server_opts)
 
         vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
           vim.lsp.diagnostic.on_publish_diagnostics, {
