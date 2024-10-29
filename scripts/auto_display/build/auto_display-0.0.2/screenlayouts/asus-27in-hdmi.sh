37d69b1d43df34c36186006b4ee72cd02d3d16fd
Elliot Wesoff <ewesoff@gmail.com>
2024-10-29 00:43:39 -0700

    remove auto_display build. whoopsie

--- scripts/auto_display/build/auto_display-0.0.2/screenlayouts/asus-27in-hdmi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-xrandr \
-  --output eDP-1 --off \
-  --output DP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --rate "144.01" \
-  --output DP-2 --off \
-  --output DP-3 --off \
-  --output DP-4 --off
