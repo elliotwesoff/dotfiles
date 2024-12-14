bb042fd22e67a681476f9cf53a7f1073c0074657
Elliot Wesoff <ewesoff@gmail.com>
2024-12-13 17:15:28 -0800

    some random changes, fix dunst notif size.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,10 +2,13 @@ local lazy = require('lazy_config')
 local settings = require('settings')
 local keymaps = require('keymaps')
 
+
 -- settings.COLORSCHEME_LIGHT = 'seoulbones'
 -- settings.COLORSCHEME_LIGHT = 'tokyobones'
-settings.COLORSCHEME_LIGHT = 'zenwritten'
-settings.COLORSCHEME_DARK = 'nordbones'
+-- settings.COLORSCHEME_LIGHT = 'zenwritten'
+-- settings.COLORSCHEME_DARK = 'nordbones'
+
+vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
 
 lazy.bootstrap()
 lazy.init()
