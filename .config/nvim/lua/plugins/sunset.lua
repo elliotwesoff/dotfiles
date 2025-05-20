81a23a70e7989e724458beaf85330aeda53f6edd
Elliot Wesoff <ewesoff@gmail.com>
2025-05-20 12:39:34 -0700

    [nvim] fix sunset and opening nvim in diff mode

--- .config/nvim/lua/plugins/sunset.lua
+++ .config/nvim/lua/plugins/sunset.lua
@@ -1,22 +1,22 @@
 return {
   'JManch/sunset.nvim',
   enabled = true,
-  event = 'BufReadPre',
   lazy = false,
-  priority = 1000,
   config = function()
     local sunset = require('sunset')
     local settings = require('settings')
+    local lat = tonumber(os.getenv('LOC_LAT'))
+    local long = tonumber(os.getenv('LOC_LONG'))
     local light_theme = settings.COLORSCHEME_LIGHT
     local dark_theme = settings.COLORSCHEME_DARK
 
-    if (string.find(light_theme, "bones") or string.find(dark_theme, "bones")) then
+    if (string.find(light_theme, 'bones') or string.find(dark_theme, 'bones')) then
       require('zenbones')
     end
 
     sunset.setup({
-      latitude = tonumber(os.getenv("LOC_LAT")),
-      longitude = tonumber(os.getenv("LOC_LONG")),
+      latitude = lat,
+      longitude = long,
       day_callback = function()
         vim.o.background = 'light'
         vim.cmd.colorscheme(light_theme)
