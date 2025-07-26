287b2a23e8c3e8f350933f21b300acb05285e3c7
Elliot Wesoff <ewesoff@gmail.com>
2025-07-26 14:13:48 -0700

    set git default editor to nvim, remove python textwidth, set hex.nvim command for itunes_db hex dumps.

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -2,3 +2,4 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.o.textwidth = 0
--- .config/nvim/lua/plugins/hex.lua
+++ .config/nvim/lua/plugins/hex.lua
@@ -2,6 +2,9 @@ return {
   'RaafatTurki/hex.nvim',
   lazy = true,
   config = function()
-    require('hex').setup()
+    require('hex').setup({
+      -- format for reading iTunesDB
+      dump_cmd = 'xxd -c 12 -g 4'
+    })
   end
 }
