228fcd52664c7a1e1991962d3e6ee64fb5fcfc44
Elliot Wesoff <ewesoff@gmail.com>
2025-08-28 23:11:25 -0700

    disable xss-lock and add comment to udev-blk.fish

--- scripts/udev-blk.fish
+++ scripts/udev-blk.fish
@@ -1,9 +1,15 @@
 #!/usr/bin/env fish
 #
 # udev-blk.fish
+#
+# this script is called from /etc/udev/rules.d/50-usb.rules
+# with `action` as one of `[add|remove|change]` and blkdev as
+# the name/id of the block device. it appends to the output file
+# so that any `notify-mon` instance watching the output file
+# can pick up on the changes and show a dunst notification.
 
 set action $argv[1]
 set blkdev $argv[2]
-set output /dev/shm/udev-blk.log
+set output /tmp/udev-blk.log
 
 echo "block device $action: $blkdev" | tee -a $output
