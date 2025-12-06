6feddb6006c36bb5f6ca5c1e2975b3c92cf14ac4
Elliot Wesoff <ewesoff@gmail.com>
2025-12-06 09:15:53 -0800

    fix location longitude env var

--- .config/fish/functions/set_coordinates.fish
+++ .config/fish/functions/set_coordinates.fish
@@ -1,11 +1,11 @@
 function set_coordinates
-    if test (timedatectl show --property=Timezone) = "Timezone=Europe/Berlin"
-        # munich
-        set -gx LOC_LAT 48.14803580815556
-        set -gx LOC_LONG 11.588875333597782
-    else
-        # surf beach
-        set -gx LOC_LAT 34.6831139
-        set -gx LOC_LONG -120.6232676
-    end
+  if test (timedatectl show --property=Timezone) = "Timezone=Europe/Berlin"
+    # munich
+    set -gx LOC_LAT 48.14803580815556
+    set -gx LOC_LONG 11.588875333597782
+  else
+    # surf beach
+    set -gx LOC_LAT 34.6831139
+    set -gx LOC_LONG "-120.6232676"
+  end
 end
